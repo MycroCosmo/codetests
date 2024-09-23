2662bc87a8e03b504a122ef6741088cbeb8b67d5
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-09-23 21:02:05 +0900

    [Silver I] Title: 회의실 배정, Time: 1064 ms, Memory: 174588 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,35 @@
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[][] meetings = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            meetings[i][0] = sc.nextInt();
+            meetings[i][1] = sc.nextInt();
+        }
+
+        Arrays.sort(meetings, (a, b) -> {
+            if (a[1] == b[1]) {
+                return a[0] - b[0];
+            }
+            return a[1] - b[1];
+        });
+
+        int count = 0;
+        int lastEndTime = 0;
+
+        for (int i = 0; i < N; i++) {
+            if (meetings[i][0] >= lastEndTime) {
+                count++;
+                lastEndTime = meetings[i][1];
+            }
+        }
+
+        System.out.println(count);
+    }
+}
